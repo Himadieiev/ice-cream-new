a3b8050755ba100e8b2bd03c8823ccc80955070b
Ruslan Himadieiev <gimadejev86@gmail.com>
2022-12-25 18:27:40 +0200

    add

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './js/btnup';
+import './js/buy-now-modal';
+import './js/ingredients-modal';
+import './js/ingredients-modal-icecoffee';
+import './js/menu';
+import './js/our-location-modal';
+import './js/read-more-modal';
