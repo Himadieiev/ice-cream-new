8db8da601d417933c9438b00ec4cc0423993327a
Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2022-12-26 22:12:51 +0000

    Deploying to gh-pages from @ Himadieiev/ice-cream-new@fa4ec1bb54631b3d120841371b2f0afe50aa53cc 🚀

---
+++ index.249c1282.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn1:document.querySelector("[data-modal-by-now-open1]"),closeModalBtn:document.querySelector("[data-modal-by-now-close]"),modal:document.querySelector("[data-modal-by-now]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn1.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.249c1282.js.map
