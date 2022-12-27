dc8c68f0fb0068646b675e2733df4cb0b254bf11
Ruslan Himadieiev <gimadejev86@gmail.com>
2022-12-27 09:35:16 +0200

    add

--- src/js/buy-now-modal.js
+++ src/js/buy-now-modal.js
@@ -1,19 +1,5 @@
 (() => {
-<<<<<<< Updated upstream
-    const refs = {
-      openModalBtn1: document.querySelector('[data-modal-by-now-open1]'),
-      closeModalBtn: document.querySelector('[data-modal-by-now-close]'),
-      modal: document.querySelector('[data-modal-by-now]'),
-    };
-  
-    refs.openModalBtn1.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle('is-hidden');
-    }
-  })();
-=======
+
   const refs = {
     openModalBtn: document.querySelector("[buy-now-modal-open]"),
     closeModalBtn: document.querySelector("[buy-now-modal-close]"),
@@ -27,4 +13,3 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
->>>>>>> Stashed changes
