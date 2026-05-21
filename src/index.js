ac9280916b34a0f686c35856adede39f61e55306
Himadieiev Ruslan <gimadejev86@gmail.com>
2026-05-21 14:55:34 +0300

    Cleanup: remove duplicate index.html, unused scripts, rename header-test to header.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-
 import './js/burger-menu';
-    import './js/mobile-menu';
-    import './js/btnup';
-    import './js/button-up';
-    import './js/buy-now-modal';
-    import './js/ingredients-modal-icecoffee';
-    import './js/ingredients-modal';
-    import './js/modal-franchise';
-    import './js/our-location-modal';
-    import './js/read-more-modal';
+import './js/button-up';
+import './js/buy-now-modal';
+import './js/ingredients-modal';
+import './js/modal-franchise';
+import './js/our-location-modal';
+import './js/read-more-modal';
--- src/js/ingredients-modal.js
+++ src/js/ingredients-modal.js
@@ -4,7 +4,7 @@
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal-ingredients]'),
   };
-  console.log(refs.openModalBtn);
+
   for (var i = 0; i < refs.openModalBtn.length; i++) {
     refs.openModalBtn[i].addEventListener('click', toggleModal);
     refs.closeModalBtn.addEventListener('click', toggleModal);
