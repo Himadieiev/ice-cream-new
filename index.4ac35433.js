c83fcd3abe09c31ed672a90cad2600a36d9524d1
Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2026-05-21 12:01:51 +0000

    Deploying to gh-pages from @ Himadieiev/ice-cream-new@6c6df76bd7f79c1844aadef113a9f3941cd904ed 🚀

--- index.4ac35433.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector("[read-more-modal-open]"),closeModalBtn:document.querySelector("[read-more-modal-close]"),modal:document.querySelector("[read-more-modal]")};function o(){e.modal.classList.toggle("is-open")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.4ac35433.js.map
--- index.efda4345.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn1:document.querySelector("[data-modal-ingredients-ice-open]"),closeModalBtn:document.querySelector("[data-modal-ingredients-ice-close]"),modal:document.querySelector("[data-modal-ingredients-ice]")};function n(){e.modal.classList.toggle("is-hidden")}e.openModalBtn1.addEventListener("click",n),e.closeModalBtn.addEventListener("click",n)}();
-//# sourceMappingURL=index.efda4345.js.map
